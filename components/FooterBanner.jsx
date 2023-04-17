afb175db2407951bf95b6b562fab2159724302e7
khuephamy <khuephumy19@gmail.com>
2023-04-17 17:32:56 -0500

    feature footer banner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -20,13 +20,19 @@ const FooterBanner = ({
   return (
     <div className="footer-banner__section">
       <div className="footer-banner__container">
-        <div className="footer-banner_left-desc">
+        <div className="footer-banner_left-con">
           <p className="footer-banner__discount">{discount}</p>
           <h3 className="footer-banner__title">{largeText1}</h3>
           <h3 className="footer-banner__title">{largeText2}</h3>
           <p className="footer-banner__sale-date">{saleTime}</p>
         </div>
-        <div className="footer-banner_right-desc"></div>
+        <img className="footer-banner__image" src={urlFor(image)} alt="product" />
+        <div className="footer-banner_right-con">
+          <p className="footer-banner__small-desc1">{smallText}</p>
+          <h3 className="footer-banner__mid-desc">{midText}</h3>
+          <h3 className="footer-banner__small-desc2">{desc}</h3>
+         <button className="footer-banner__button">{buttonText}</button>
+        </div>
       </div>
     </div>
   );
