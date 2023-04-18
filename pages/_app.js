4f645018d88e18862422dec248b0605a8dfe4b14
khuephamy <khuephumy19@gmail.com>
2023-04-18 18:59:35 -0500

    organized stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 import '@/styles/globals.css'
+import '@/styles/Header.css'
+import '@/styles/NavBar.css'
+import '@/styles/HeroBanner.css'
+import '@/styles/Main.css'
+import '@/styles/ProductCard.css'
+import '@/styles/FooterBanner.css'
+import '@/styles/AccountModal.css'
+import '@/styles/Footer.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
