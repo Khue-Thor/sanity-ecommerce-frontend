01f1ab3cb4a5b9436dd8a215fc91d53733867e12
khuephamy <khuephumy19@gmail.com>
2023-04-18 15:53:57 -0500

    product image styles

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,21 +1,63 @@
 import React from "react";
-
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { client, urlFor } from "../../lib/client";
 
 const ProductDetails = ({ product, products }) => {
-  const { image, name, details, price } = product;
+  const { image, name, desc, price } = product;
 
   return (
     <div className="product">
       <div className="product__detail-container">
-        <div className="product__image">
-          <img src={urlFor(image && image[0])} />
+        <div>
+          <div className="product__image-container">
+            <img src={urlFor(image && image[0])} className="product__image"/>
+          </div>
+          {/* <div className="product__small-image-container">
+            {image?.map((item, i) => (
+              <img src={urlFor(item)} className="" onMouseEnter=""/>
+            ))}
+            </div> */}
+        </div>
+        <div className="product__detail-desc">
+          <h4>Details:</h4>
+
+          <p>{desc}</p>
+          <div>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <p>${price}</p>
+          <div className="product__quantity">
+            <h3>Quantity:</h3>
+            <p>
+              <span>
+                <AiOutlineMinus />
+              </span>
+              <span>0</span>
+              <span>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div>
+            <button type="button" className="product__add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="product__add-to-cart" onClick="">
+              Buy Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 // getStaticPaths is needed, if not the getStaticProps won't work.
 
