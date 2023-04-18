14166989ab8376b29aa68087eebd35820aa296dc
khuephamy <khuephumy19@gmail.com>
2023-04-18 14:16:31 -0500

    feature getStaticProps

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 npx create-next-app
 npm install --legacy-peer-deps
+
+
+
+use [slug] the [] mean it gonna be dinamic change
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { client, urlFor } from '../../lib/client'
+
 const ProductDetails = () => {
   return (
-    <div>ProductDetails</div>
+    <div>
+      <div className='product-detail__container'>
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
+
+
+export const getStaticProps = async ({params: { slug }}) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '[_type == "product"]';
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery)
+
+  return {
+    props: { products, product },
+  };
+};
 
 export default ProductDetails
