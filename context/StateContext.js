8bf8dc54803f48a4939be61276444523b5d7c1ef
khuephamy <khuephumy19@gmail.com>
2023-04-19 18:51:52 -0500

    create context

---
+++ context/StateContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { Toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const[totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider value={{
+      showCart,
+      cartItems,
+      totalPrice,
+      totalQuantities,
+    }}>
+      {children}
+    </Context.Provider>
+  )
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
 import '@/styles/globals.css';
-import '@/styles/Header.css';
-import '@/styles/NavBar.css';
-import '@/styles/HeroBanner.css';
-import '@/styles/Main.css';
-import '@/styles/ProductCard.css';
-import '@/styles/FooterBanner.css';
-import '@/styles/AccountModal.css';
-import '@/styles/Footer.css';
-import '@/styles/Slug.css';
-import '@/styles/RecProduct.css';
+
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Toaster/>
+      <Component {...pageProps} />
+    </StateContext>
+  )
 }
