34131c197661db2e99ad7b5e301ed15d950e2b5a
khuephamy <khuephumy19@gmail.com>
2023-04-24 11:24:44 -0500

    feature cart inc/decr and price

--- pages/cart.js
+++ pages/cart.js
@@ -43,6 +43,27 @@ const Cart = () => {
                   <img src={urlFor(item?.image[0])} className="cart__product-image" />
                   <div className="cart__product-desc-container">
                     <p className="cart__product-desc">{item.desc}</p>
+                    <div className="cart__btn_price">
+                      <div className="product__quantity">
+                        <h3 className="product__quantity-title">Quantity:</h3>
+                        <p className="product__quantity-button-container">
+                          <span
+                            className="product__quantity-button decrease-button"
+                            onClick={decreaseQty}
+                          >
+                            <AiOutlineMinus className="button-icon" />
+                          </span>
+                          <span className="quantity_count">{qty}</span>
+                          <span
+                            className="product__quantity-button increase-button "
+                            onClick={increaseQty}
+                          >
+                            <AiOutlinePlus className="button-icon" />
+                          </span>
+                        </p>
+                      </div>
+                      <p className="cart__checkout-total-price">${item.price}</p>
+                    </div>
                   </div>
                 </div>
               ))}
@@ -51,7 +72,7 @@ const Cart = () => {
         <div className="cart__checkout-wrapper">
           <p className="cart__checkout-total">
             Subtotal ({totalQuantities} items):{" "}
-            <span className="cart__checkout-total-price">${totalPrice}29.99</span>
+            <span className="cart__checkout-total-price">${totalPrice}</span>
           </p>
           <button type="button" className="cart__checkout-button">
             Proceed to Checkout
