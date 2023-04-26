0be2ea24c17afeb55af9403f5e001ba0793df74d
khuephamy <khuephumy19@gmail.com>
2023-04-26 09:50:43 -0500

    update stripe sucess/cancel url path

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           };
         }),
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        success_url: `${req.headers.origin}/`,
+        cancel_url: `${req.headers.origin}/`,
       };
 
       // Create Checkout Sessions from body params.
