51269dd90fcad78cc3ffbeb1b2dea45faa971b72
khuephamy <khuephumy19@gmail.com>
2023-04-26 15:22:23 -0500

    update signin css

--- pages/signin.js
+++ pages/signin.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 
 const Signin = ({ email, handleEmail, password, handlePassword }) => {
   return (
     <div className="sign-in__page">
-      <div className="sign-in__modal">
+      <div className="sign-in__modal-con">
+        <Link href={'/'}>
+          <BsFillArrowLeftCircleFill className="sign-in__page-back-arrow"/>
+        </Link>
         <fieldset className="modal__form-fieldset">
           <h2>Sign in:</h2>
           <label className="modal__form-label">Email or Phone number</label>
@@ -36,7 +40,7 @@ const Signin = ({ email, handleEmail, password, handlePassword }) => {
           </button>
         </fieldset>
         <p className="sign-in__page-ques">new customer?</p>
-        <Link href={'/signup'}>
+        <Link href={"/signup"}>
           <button type="button" className="modal-sign-up__btn modal-btn">
             Sign up
           </button>
